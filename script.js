bd2eee3f95d682710e7bd23a237c84d12d9d7dbc
Lauren Joseph <ljoseph.student@hsfi.us>
2017-01-17 21:50:09 +0000

    simely face, turning courners

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //Write your function calls below this line
+/*
+Simely Face
+setColor(2,2, "black");
+setColor(2,6, "black");
+setColor(5,1, "black");
+setColor(6,2, "black");
+setColor(7,3, "black");
+setColor(7,4, "black");
+setColor(7,5, "black");
+setColor(6,6, "black");
+setColor(5,7, "black");
+*/
+/*
+Turning Courners
+setColor(0,0, "yellow");
+setColor(0,9, "yellow");
+setColor(9,0, "yellow");
+setColor(9,9, "yellow");
+*/
+setColor(1,2, "purple");
+setColor(1,3, "purple");
+setColor(1,4, "purple");
+setColor(1,5, "purple");
+setColor(1,6, "purple");
+setColor(3,1, "purple");
+setColor(3,2, "purple");
+setColor(3,3, "purple");
+setColor(3,4, "purple");
+setColor(3,5, "purple");
+setColor(3,6, "purple");
+setColor(3,7, "purple");
+
+
+
+setColor(2,1, "purple");
